a6a2b894a22b1a1ace5bfadc126ec0506da60020
www <152867632+Jsjst1@users.noreply.github.com>
2024-09-08 14:57:37 +0200

    Initial commit

---
+++ start.bat
@@ -0,0 +1,6 @@
+cd mcsmanager
+cd daemon
+start node_app.exe --enable-source-maps --max-old-space-size=8192 app.js 
+ping localhost
+cd ../web
+start node_app.exe --enable-source-maps --max-old-space-size=8192 app.js --open
